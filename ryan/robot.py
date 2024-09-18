ee780ee39695c76d093a33cacc32f5df8041085c
Ryan Stewart <rste0012@student.monash.edu>
2024-09-19 09:56:39 +1000

    replaced robot.py with will's new code

--- ryan/robot.py
+++ ryan/robot.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from flask import Flask, Response
 import cv2 as cv
 from vision import vision
@@ -16,32 +17,23 @@ class Robot:
         self.R = []
         self.image_center_threshold = 20 # so that the turn towards algorithm does not get stuck in an infinite loop 
         self.ser = Serial('/dev/serial0', 9600, timeout=1) # start serial instance for Robot
+=======
+import cv2
+import numpy as np
+import random
+import time
 
-    def encode_frame(self, frame):
-        success, buffer = cv.imencode('.jpg', frame)
-        if not success:
-            print("Failed to encode")
-            return None, False
-        return buffer.tobytes(), True
+import serial
+>>>>>>> Stashed changes
 
-    def app_route(self):
-        @self.app.route('/')
-        def index():
-            print("Page requested")
-            return '''
-            <!doctype html>
-            <html>
-            <body>
-                <img src="/video_feed">
-            </body>
-            </html>
-            '''
-        
-        @self.app.route('/video_feed')
-        def video_feed():
-            frame_packet = self.generate_frame()
-            return Response(frame_packet, mimetype='multipart/x-mixed-replace; boundary=frame')
+cam_id = 0
+use_uart = True
+use_ui = False
+use_video = False
 
+frame_mod = 15
+
+<<<<<<< Updated upstream
     def draw_shapes(self, frame):
         for i in range(len(self.R)):
             radius = self.R[i]
@@ -125,6 +117,162 @@ class Robot:
         
         
             
+=======
+if use_uart:
+    ser = serial.Serial('/dev/serial0', 9600, timeout=1)
+
+if __name__ == '__main__':
+
+    vid = cv2.VideoCapture(cam_id)
+    vid.set(cv2.CAP_PROP_FRAME_WIDTH, 640) # 1280
+    vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 480) # 720
+
+    prev_frame_pos = None
+    close_to_ball = False
+    s_count = 0
+    l_count = 0
+    r_count = 0
+    f_count = 0
+    frame_count = 0
+    c_count = 0
+
+    if use_video:
+        video_path = 'indoor_pic/4.mp4'
+        vid = cv2.VideoCapture(video_path)
+
+    while True:
+        data = "SSSSSSSSSSSSSS"
+        ret, frame = vid.read()
+        #frame = cv2.imread('indoor_pic/3.jpg')
+        if close_to_ball:
+            print("adjusting...")
+
+        if not ret:
+            if use_video:
+                vid = cv2.VideoCapture(video_path)
+            else: 
+                vid = cv2.VideoCapture(cam_id)
+                vid.set(cv2.CAP_PROP_FRAME_WIDTH, 640) # 1280
+                vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 480) # 720
+
+            ret, frame = vid.read()
+        #frame = cv2.imread('indoor_pic/8.jpg')
+
+        # Define the lower and upper boundaries of the "tennis ball" in the HSV color space
+        lower_yellow = np.array([22, 47, 50])
+        upper_yellow = np.array([60, 200, 255])
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+        mask = cv2.erode(mask, None, iterations=2)
+        mask = cv2.dilate(mask, None, iterations=2)
+        contours, _ = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        bottom_half = []
+        if len(contours) > 0:
+            # Find the largest contour in the mask, then use it to compute the minimum enclosing circle and centroid
+            #c = max(contours, key=cv2.contourArea)
+            bottom_half = []
+            for c in contours:
+                ((x, y), radius) = cv2.minEnclosingCircle(c)
+                if y > 260: 
+                    if radius > 1:
+                        bottom_half.append(c)
+
+        if bottom_half:
+            c = max(bottom_half, key=cv2.contourArea)
+            M = cv2.moments(c)
+            ((x, y), radius) = cv2.minEnclosingCircle(c)
+            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+            ignore = False
+            #print(radius)
+            if prev_frame_pos is not None:
+                x_old, y_old = prev_frame_pos
+                dist = np.sqrt((x_old - x)**2 + (y_old -y)**2)
+
+                if dist > 15 or dist == 0:
+                    ignore = True
+            prev_frame_pos = (x,y)
+            if use_ui and not ignore:
+                cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+                cv2.circle(frame, center, 5, (0, 0, 255), -1)
+>>>>>>> Stashed changes
             
+            	#print(radius)    
+                if radius > 200:
+            	    print("close but not stopping")
             
+            if use_uart and not ignore:
+                if close_to_ball:
+                    image_center_threshold = 30
+                else:
+                    image_center_threshold = 120
+                direction = "S"
+                if x-320 < -image_center_threshold:
+                    direction = "L"
+                    l_count += 1
+                elif x-320 > image_center_threshold:
+                    direction = "R"
+                    r_count += 1
+                else:
+                    if not close_to_ball:
+                        if radius > 140:
+                            direction = "C"
+                            print("CLOSE!!!!!")
+                            c_count += 1
+                        else:
+                            direction = "F"
+                            f_count += 1
+                    else:
+                        direction = "W"
+                data = ""
+                for i in range(10):
+                    data += direction
         
+        if use_uart:
+            frame_count+=1
+
+            if data[0] == "S":
+                s_count+=1
+
+            if c_count >= 3:
+                close_to_ball = True
+            
+            if frame_count == frame_mod and not close_to_ball:
+                frame_count = 0
+                counts = [s_count, f_count, l_count, r_count, c_count]
+                most_common = np.argmax(counts)
+                datas = ["SSSSSSSSSS", "FFFFFFFFFF", "LLLLLLLLLL", "RRRRRRRRRR", "CCCCCCCCCC"]
+                
+                data = datas[most_common]
+
+                s_count = 0
+                f_count = 0
+                l_count = 0
+                r_count = 0
+                c_count = 0
+
+                ser.write(data.encode('utf-8'))
+                print(data[0])
+                
+                if data[0] == "C":
+                    close_to_ball = True
+
+            if close_to_ball:
+                ser.write(data.encode('utf-8')) #send the command over UART to the STM
+                ser.write(data.encode('utf-8'))
+                ser.write(data.encode('utf-8'))
+                print(data[0])
+                if data[0] == "W":
+                    print("Reached the END!!")
+                    break
+
+        #print(x-320,y)
+        if use_ui:
+            cv2.imshow("Frame", frame)
+            cv2.imshow("mask", mask)
+        #cv2.moveWindow('Original', x_offset * 2-150, y_offset * 2)
+        #time.sleep(0.05)
+
+        if use_ui:
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
---
+++ ryan/robot_old.py
@@ -0,0 +1,142 @@
+from flask import Flask, Response
+import cv2 as cv
+from tennis_detect import tennis_detector
+import time
+import numpy as np
+from serial import Serial
+from vision import vision 
+
+class Robot:
+    def __init__(self, cap, port=8080): 
+        self.cap = cap
+        self.port = port
+        self.app = Flask(__name__)
+        self.app_route()
+        #self.tennis_detector = tennis_detector(length_filter=30, remove_div_points = 8, random_points=20, threshold=4)
+        self.vision = vision()
+        self.C = []
+        self.x = 0
+        self.y = 0
+        self.R = []
+        self.image_center_threshold = 20 # so that the turn towards algorithm does not get stuck in an infinite loop 
+        self.ser = Serial('/dev/serial0', 9600, timeout=1) # start serial instance for Robot
+
+    def encode_frame(self, frame):
+        success, buffer = cv.imencode('.jpg', frame)
+        if not success:
+            print("Failed to encode")
+            return None, False
+        return buffer.tobytes(), True
+
+    def app_route(self):
+        @self.app.route('/')
+        def index():
+            print("Page requested")
+            return '''
+            <!doctype html>
+            <html>
+            <body>
+                <img src="/video_feed">
+            </body>
+            </html>
+            '''
+        
+        @self.app.route('/video_feed')
+        def video_feed():
+            frame_packet = self.generate_frame()
+            return Response(frame_packet, mimetype='multipart/x-mixed-replace; boundary=frame')
+
+    def draw_shapes(self, frame):
+        x, y = self.C
+        # Draw the circle around the detected object
+        cv.circle(frame, (int(x), int(y)), int(self.R), (0, 255, 255), 2)
+        # Draw the center of the detected object
+        cv.circle(frame, self.C, 5, (0, 0, 255), -1)
+        return frame
+
+     
+    def generate_object_coordinates(self):
+        lower_yellow = np.array([29,86,6]) 
+        upper_yellow = np.array([64,255,255]) 
+        while True:
+            success, frame = self.cap.read()
+            if not success: 
+                break
+            hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+            mask = cv.inRange(hsv, lower_yellow, upper_yellow)
+            mask = cv.erode(mask, None, iterations=2)
+            mask = cv.dilate(mask, None, iterations=2)
+            contours, _ = cv.findContours(mask.copy(), cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+            
+            if len(contours)>0:
+                c = max(contours, key=cv.contourArea)
+                ((x, y), radius) = cv.minEnclosingCircle(c)
+                self.x = x
+                self.y = y # comment this out later cos its jank
+                M = cv.moments(c)
+                center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+                if radius > 10:
+                    self.C = center
+                    self.R = radius
+            time.sleep(0.1) 
+
+    def cast_frame(self,frame): 
+        # Decrease the brightness by subtracting the value
+        value = 0
+        # Convert the image to float32 to prevent clipping issues
+        frame = frame.astype(np.float32)
+        # Decrease the brightness by subtracting the value
+        frame -= value
+        # Clip the values to be in the valid range [0, 255] and convert back to uint8
+        frame = np.clip(frame, 0, 255).astype(np.uint8)
+        return frame 
+        
+    
+    def generate_frame(self):
+        while True:
+            #reduce the frame rate significantly to reduce CPU strain. Can do this because it is just  GUI and not important for the robots function
+            time.sleep(0.02)
+            success, frame = self.cap.read()
+            if not success:
+                print("Could not get frame")
+                break
+            # cast the frame into a standard size 
+            frame = self.cast_frame(frame)
+            # draw the object detection shapes on the frame
+            frame = self.draw_shapes(frame)
+            #encode the frame 
+            frame_encoded, success = self.encode_frame(frame)
+            if success:
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame_encoded + b'\r\n')
+            else:
+                print("Could not generate frame packet")
+
+    def run_server(self):
+        print("Starting server")
+        self.app.run(host='0.0.0.0', port=self.port)
+        
+        
+    def movement_control(self): 
+        while True: 
+            x,y = self.C
+            direction = "S" # default stop  
+            if x < -self.image_center_threshold:
+                direction = "L"
+            elif x > self.image_center_threshold:
+                direction = "R"
+            else: 
+                direction = "F"     
+            data = ""
+            # I am not sure what the loop is for
+            for i in range(10):
+                data += direction
+            self.ser.write(data.encode('utf-8')) #send the command over UART to the STM
+            print(f"{direction}")
+                        
+        
+        
+            
+            
+            
+        
