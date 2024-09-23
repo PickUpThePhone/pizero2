188e39117a697519b32fa0837b9cce2f0fe7930f
PickUpThePhone <rste0012@student.monash.edu>
2024-09-23 20:48:04 +1000

    Added wills M1 movement code to the gui code

--- ryan/robot.py
+++ ryan/robot.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 from flask import Flask, Response
 import cv2 as cv
 from vision import vision
@@ -17,23 +16,7 @@ class Robot:
         self.R = []
         self.image_center_threshold = 20 # so that the turn towards algorithm does not get stuck in an infinite loop 
         self.ser = Serial('/dev/serial0', 9600, timeout=1) # start serial instance for Robot
-=======
-import cv2
-import numpy as np
-import random
-import time
 
-import serial
->>>>>>> Stashed changes
-
-cam_id = 0
-use_uart = True
-use_ui = False
-use_video = False
-
-frame_mod = 15
-
-<<<<<<< Updated upstream
     def draw_shapes(self, frame):
         for i in range(len(self.R)):
             radius = self.R[i]
@@ -90,189 +73,78 @@ frame_mod = 15
         
         
     def movement_control(self): 
+        # init
+        l_count = 0
+        r_count = 0 
+        f_count = 0
+        # counter for num instances a ball has been detected in a row
+        inst = 0
+        image_center_threshold = 120 
         while True: 
-            if len(self.R)>0: 
-                # only if radius is large enough 
+
+
+            #If there is at least one ball detected 
+            if len(self.R)>0:
+                inst+=1
+                # get the radius of the closest ball (by default only the closest is saved in this array)
                 radius = self.R[0]
-                if radius > 10:
-                    x,_ = self.C[0]
-                    # define an array of x,y coordinactually need the y coordinate but its good to have as we may need it later
-                    # turn robot towards those coordinates
-                    direction = "S" # default stop  
-                    if x-320 < -self.image_center_threshold:
-                        direction = "L"
-                    elif x-320 > self.image_center_threshold:
-                        direction = "R"
-                    else: 
-                        direction = "F"     
-                    data = ""
-                    # I am not sure what the loop is for
-                    print(direction)
-                    for i in range(10):
-                        data += direction
-                    self.ser.write(data.encode('utf-8')) #send the command over UART to the STM
-                    # wait 
-                    time.sleep(0.02) 
-            
-        
-        
-            
-=======
-if use_uart:
-    ser = serial.Serial('/dev/serial0', 9600, timeout=1)
-
-if __name__ == '__main__':
-
-    vid = cv2.VideoCapture(cam_id)
-    vid.set(cv2.CAP_PROP_FRAME_WIDTH, 640) # 1280
-    vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 480) # 720
-
-    prev_frame_pos = None
-    close_to_ball = False
-    s_count = 0
-    l_count = 0
-    r_count = 0
-    f_count = 0
-    frame_count = 0
-    c_count = 0
-
-    if use_video:
-        video_path = 'indoor_pic/4.mp4'
-        vid = cv2.VideoCapture(video_path)
-
-    while True:
-        data = "SSSSSSSSSSSSSS"
-        ret, frame = vid.read()
-        #frame = cv2.imread('indoor_pic/3.jpg')
-        if close_to_ball:
-            print("adjusting...")
-
-        if not ret:
-            if use_video:
-                vid = cv2.VideoCapture(video_path)
-            else: 
-                vid = cv2.VideoCapture(cam_id)
-                vid.set(cv2.CAP_PROP_FRAME_WIDTH, 640) # 1280
-                vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 480) # 720
-
-            ret, frame = vid.read()
-        #frame = cv2.imread('indoor_pic/8.jpg')
-
-        # Define the lower and upper boundaries of the "tennis ball" in the HSV color space
-        lower_yellow = np.array([22, 47, 50])
-        upper_yellow = np.array([60, 200, 255])
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
-        mask = cv2.erode(mask, None, iterations=2)
-        mask = cv2.dilate(mask, None, iterations=2)
-        contours, _ = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        bottom_half = []
-        if len(contours) > 0:
-            # Find the largest contour in the mask, then use it to compute the minimum enclosing circle and centroid
-            #c = max(contours, key=cv2.contourArea)
-            bottom_half = []
-            for c in contours:
-                ((x, y), radius) = cv2.minEnclosingCircle(c)
-                if y > 260: 
-                    if radius > 1:
-                        bottom_half.append(c)
-
-        if bottom_half:
-            c = max(bottom_half, key=cv2.contourArea)
-            M = cv2.moments(c)
-            ((x, y), radius) = cv2.minEnclosingCircle(c)
-            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-            ignore = False
-            #print(radius)
-            if prev_frame_pos is not None:
-                x_old, y_old = prev_frame_pos
-                dist = np.sqrt((x_old - x)**2 + (y_old -y)**2)
-
-                if dist > 15 or dist == 0:
-                    ignore = True
-            prev_frame_pos = (x,y)
-            if use_ui and not ignore:
-                cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
-                cv2.circle(frame, center, 5, (0, 0, 255), -1)
->>>>>>> Stashed changes
-            
-            	#print(radius)    
-                if radius > 200:
-            	    print("close but not stopping")
-            
-            if use_uart and not ignore:
-                if close_to_ball:
+                # get ball center x coordinate
+                center_x, _ = self.C[0]
+                # get pixel dist from center 
+                dist_from_center = center_x - 320
+                
+                #======================================================================
+                
+                # If the ball is far away: (determined by a small ball radius)
+                if (radius < 140):
+                    image_center_threshold = 120 
+                # if the ball is in medium distance 
+                elif (140 <= radius < 200): 
                     image_center_threshold = 30
-                else:
-                    image_center_threshold = 120
-                direction = "S"
-                if x-320 < -image_center_threshold:
-                    direction = "L"
-                    l_count += 1
-                elif x-320 > image_center_threshold:
-                    direction = "R"
-                    r_count += 1
-                else:
-                    if not close_to_ball:
-                        if radius > 140:
-                            direction = "C"
-                            print("CLOSE!!!!!")
-                            c_count += 1
-                        else:
-                            direction = "F"
-                            f_count += 1
-                    else:
-                        direction = "W"
-                data = ""
-                for i in range(10):
-                    data += direction
-        
-        if use_uart:
-            frame_count+=1
+                # if the ball is close (radius larger than 200 pixels)
+                else: 
+                    # stop immediately 
+                    stop = "SSSSSSSSSS"
+                    self.ser.write(stop.encode('utf-8'))
+                    print(f"Ball is close. Halt movement.\n")
+                    # restart the loop 
+                    continue
 
-            if data[0] == "S":
-                s_count+=1
+                #=====================================================================
 
-            if c_count >= 3:
-                close_to_ball = True
-            
-            if frame_count == frame_mod and not close_to_ball:
-                frame_count = 0
-                counts = [s_count, f_count, l_count, r_count, c_count]
-                most_common = np.argmax(counts)
-                datas = ["SSSSSSSSSS", "FFFFFFFFFF", "LLLLLLLLLL", "RRRRRRRRRR", "CCCCCCCCCC"]
+                # take a vote between L, R, F commands 
+                if dist_from_center < -image_center_threshold:
+                    # vote that the robot drives left
+                    l_count+=1
+                elif dist_from_center > image_center_threshold:
+                    # vote that the robot drives right 
+                    r_count+=1
+                else: 
+                    # vote that the robot drives forward 
+                    f_count+=1
                 
-                data = datas[most_common]
-
-                s_count = 0
-                f_count = 0
-                l_count = 0
-                r_count = 0
-                c_count = 0
-
-                ser.write(data.encode('utf-8'))
-                print(data[0])
                 
-                if data[0] == "C":
-                    close_to_ball = True
+                #======================================================================
 
-            if close_to_ball:
-                ser.write(data.encode('utf-8')) #send the command over UART to the STM
-                ser.write(data.encode('utf-8'))
-                ser.write(data.encode('utf-8'))
-                print(data[0])
-                if data[0] == "W":
-                    print("Reached the END!!")
-                    break
+                # While loop has detected a ball 3 instances in a row, and now the vote takes place on which direction to move
+                if inst>=3:
+                    # choose the direction that has the highest votes 
+                    counts = [l_count, r_count, f_count]
+                    directions = ["LLLLLLLLLL", "RRRRRRRRRR", "FFFFFFFFFF"]
+                    max_idx = counts.index(max(counts))
+                    direction = directions[max_idx]
+                    # drive toward that direction  
+                    self.ser.write(direction.encode('utf-8')) #send the command over UART to the STM
+                    print(direction)
 
-        #print(x-320,y)
-        if use_ui:
-            cv2.imshow("Frame", frame)
-            cv2.imshow("mask", mask)
-        #cv2.moveWindow('Original', x_offset * 2-150, y_offset * 2)
-        #time.sleep(0.05)
 
-        if use_ui:
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+            # ================================================================================================
+            # CURRENTLY THIS TELLS THE ROBOT TO STOP WHEN THERE IS NO BALL DETECTED. WE WILL NEED TO DECIDE WHAT HAPPENS WHEN IT CANT SEE ANYTHING
+            else:
+                stop = "SSSSSSSSSS"
+                self.ser.write(stop.encode('utf-8'))
+                print(f"No ball detected")
 
+            #=====================================================
+            # sleep for 20ms to save resources 
+            time.sleep(0.02)
